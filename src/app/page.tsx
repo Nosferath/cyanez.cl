31fa321c74410e8739fc30e5cdf6253da2bfa52f
Claudio Yáñez <epicrealms@gmail.com>
2025-07-13 12:40:17 -0400

    Agrega información personal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,14 @@ import {
   Footer, 
   Section,
   Quote,
-  // Card, 
-  // CardGrid, 
-  // Button, 
-  // ButtonGroup, 
+  Card, 
+  CardGrid, 
+  Button, 
+  ButtonGroup, 
   Text 
 } from '@/components/ui';
 import { ThemeToggleButton } from '@/components/ThemeSwitcher';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -28,40 +29,51 @@ export default function Home() {
         </div>
         
         <main className='section-content'>
-          <Section title="🚧 Página en construcción 🚧">
-            <Quote author="Tim Urban">
-              A remarkable, glorious achievement is just what a long series of unremarkable, unglorious tasks looks like from far away.
-            </Quote>
-          </Section>
-        </main>
-
-        {/* <main className="section-content">
-          <Section title="ABOUT">
+          <Quote author="Tim Urban">
+            A remarkable, glorious achievement is just what a long series of unremarkable, unglorious tasks looks like from far away.
+          </Quote>
+          <Section title="ACERCA DE MÍ">
             <Text size="large">
-              I build digital experiences with a focus on performance,
-              accessibility, and clean code. Currently specializing in React,
-              TypeScript, and modern web technologies.
+              Sólo a través de la verdadera <b>COMPRENSIÓN</b> de los
+              problemas es posible <b>CREAR</b> soluciones que <b>CONECTEN</b> con
+              la realidad.<br /><br />
+              Esa es mi filosofía de trabajo.
             </Text>
           </Section>
-
-          <Section title="WORK">
+          
+          <Section title="EXPERIENCIA LABORAL">
             <CardGrid>
               <Card>
-                <h3 className="heading-tertiary">Current Position</h3>
-                <Text>Company Name • 2022–Present</Text>
+                <h3 className="heading-tertiary">Desarrollador Full-stack</h3>
+                <Text>Creditú • 2022–Presente</Text>
               </Card>
               <Card>
-                <h3 className="heading-tertiary">Previous Position</h3>
-                <Text>Another Company • 2020–2022</Text>
+                <h3 className="heading-tertiary">Ingeniero de I+D</h3>
+                <Text>TOC Biometrics • 2019–2021</Text>
               </Card>
             </CardGrid>
           </Section>
-
+          
+          <Section title="PUBLICACIONES">
+            <CardGrid>
+              <Card>
+                <h3 className="heading-tertiary">Impact of Occlusion Masks on Gender Classification from Iris Texture</h3>
+                <Text>Claudio Yáñez, Juan E. Tapia, Claudio A. Perez and Christoph Busch • IET Biometrics, 2024</Text>
+                <Link href="https://doi.org/10.1049/2024/8526857" rel="noopener noreferrer" target="_blank"><Text>https://doi.org/10.1049/2024/8526857</Text></Link>
+              </Card>
+              <Card>
+                <h3 className="heading-tertiary">Image Quality Assessment on Identity Documents</h3>
+                <Text>Claudio Yáñez and Juan Tapia • BIOSIG, 2021</Text>
+                <Link href="https://doi.org/10.1109/BIOSIG52210.2021.9548294" rel="noopener noreferrer" target="_blank"><Text>https://doi.org/10.1109/BIOSIG52210.2021.9548294</Text></Link>
+              </Card>
+            </CardGrid>
+          </Section>
+          
           <Section title="CONTACTO">
             <ButtonGroup>
-              <Button href="mailto:hello@example.com">
+              {/* <Button href="mailto:hello@example.com">
                 Email
-              </Button>
+              </Button> */}
               <Button 
                 href="https://github.com/Nosferath"
                 target="_blank"
@@ -78,7 +90,7 @@ export default function Home() {
               </Button>
             </ButtonGroup>
           </Section>
-        </main> */}
+        </main>
 
         <Footer>
           <Text>© 2025 Claudio Yáñez -- Construido con React y Tailwind</Text>
